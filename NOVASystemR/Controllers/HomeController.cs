801f141a732ddf8bd67c8609f8638b2f206bf2a4
Christian Mtz <christianmtz2017@outlook.com>
2023-05-24 23:27:51 -0600

    update

--- NOVASystemR/Controllers/HomeController.cs
+++ NOVASystemR/Controllers/HomeController.cs
@@ -79,6 +79,60 @@ namespace NOVASystemR.Controllers
         }
 
 
+        //[AllowAnonymous]
+        //[HttpPost]
+        //public ActionResult Login(LoginModel model, string returnUrl)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Entidades.SAI.Usuario oUsuario = new Entidades.SAI.Usuario();
+
+        //        model.AutenticacionCorrecta = LogicaNegocio.WS_SAI.Seguridad.AutenticarUsuario(model.Dominio, model.CuentaRed, model.Contrasenia);
+
+        //        if (model.AutenticacionCorrecta)
+        //        {
+        //            if (Request.UrlReferrer.Query.Contains("red") && (Request.Url.AbsoluteUri.Contains("localhost") || Request.Url.AbsoluteUri.Contains("termxnvadb14/Nova/RRHH/SRH")))
+        //            {
+        //                var a = Request.UrlReferrer.Query.Split('&');
+        //                for (var i = 0; i < a.Length; i++)
+        //                {
+        //                    if (a[i].ToString().Contains("red"))
+        //                    {
+        //                        var k = a[i].ToString().Split('=');
+        //                        model.CuentaRed = k[1];
+        //                    }
+        //                }
+        //            }
+        //            oUsuario = LogicaNegocio.WS_SAI.Seguridad.ObtenerPermisos(model.CuentaRed);
+        //            oUsuario.AutenticacionCorrecta = model.AutenticacionCorrecta;
+        //            Session["AutenticacionCorrecta"] = model.AutenticacionCorrecta;
+
+        //            List<PermisosModel> Permisos = Comun.ConvertToList<PermisosModel>(oUsuario.Permisos);
+        //            if (oUsuario.Permisos.Rows.Count > 0)
+        //                Session["Permisos"] = Permisos;
+
+        //            oUsuario.UsuarioId = (LogicaNegocio.NovaRH.Honorarios.Personal.Consultar(SqlOpciones.Actual, 0, string.Empty, 0, 0, model.CuentaRed).FirstOrDefault() ?? new Entidades.NovaRH.Honorarios.Personal() { PersonalId = oUsuario.UsuarioId }).PersonalId;
+
+        //            SetUsuarioId(oUsuario.UsuarioId);
+        //            SetNombreUsuario(oUsuario.NombreCompleto);
+
+        //            Session["Foto"] = oUsuario.Foto as byte[];
+
+        //            FormsAuthentication.SetAuthCookie(model.CuentaRed, false);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        else
+        //        {
+        //            model.AutenticacionCorrecta = false;
+        //            return View(model);
+        //        }
+        //    }
+
+        //    return View(model);
+        //}
+
+
+
         [AllowAnonymous]
         public ActionResult Logout()
         {
