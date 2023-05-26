76e67bd5015ec5caa1ae9f73b0611493c85190d6
ChristianMarH <124711580+ChristianMarH@users.noreply.github.com>
2023-05-26 15:07:43 -0600

    Update AddCampo

--- NOVASystemR/Scripts/RecetaMedica.js
+++ NOVASystemR/Scripts/RecetaMedica.js
@@ -1,26 +1,63 @@
-﻿alert('cargado');
-
-// Obtener referencias de los elementos
+﻿
 var btnAgregar = document.getElementById('btnAgregar');
 var trNuevo = document.getElementById('trNuevo');
-var contador = 4; // El número de la última línea de prescripción agregada
+var contador = 4;
+var btnImprimir = document.getElementById('btnImprimir');
+var txtCveFamiliar = document.getElementById('txtCveFamiliar');
+var txtNoSocio = document.getElementById('txtNoSocio');
 
-// Mostrar el botón 'Agregar otra línea' después de llenar la última línea de prescripción
-document.getElementById('txtPrescripcion4').addEventListener('input', function () {
-    if (this.value.trim() !== '') {
-        btnAgregar.style.display = 'block';
-    } else {
-        btnAgregar.style.display = 'none';
+
+function verificarLlenadoPrescripciones() {
+    var todasLlenas = true;
+    for (var i = 1; i <= contador; i++) {
+        var prescripcion = document.getElementById(`txtPrescripcion${i}`);
+        if (prescripcion.value.trim() === '') {
+            todasLlenas = false;
+            break;
+        }
     }
-});
+    if (todasLlenas) {
+        btnAgregar.style.display = 'none';
+    } else {
+        btnAgregar.style.display = 'block';
+    }
+}
 
-// Agregar una nueva línea de prescripción al hacer clic en el botón 'Agregar otra línea'
+
+document.getElementById('txtPrescripcion4').addEventListener('input', verificarLlenadoPrescripciones);
+
+
 btnAgregar.addEventListener('click', function () {
     contador++;
     var nuevaLinea = document.createElement('tr');
     nuevaLinea.innerHTML = `
-            <td><input type="text" class="form-control" id="txtCantSurt${contador}" placeholder="Cantidad a surtir"></td>
-            <td><textarea class="form-control no-resize" id="txtPrescripcion${contador}" rows="1" placeholder="Prescripción médica"></textarea></td>
-        `;
+        <td><input type="text" class="form-control" id="txtCantSurt${contador}" placeholder="Cantidad a surtir"></td>
+        <td><textarea class="form-control no-resize" id="txtPrescripcion${contador}" rows="1" placeholder="Prescripción médica"></textarea></td>
+    `;
     trNuevo.parentNode.insertBefore(nuevaLinea, trNuevo);
+    verificarLlenadoPrescripciones();
+
+ 
+/*    btnAgregar.style.display = 'none';*/
 });
+
+btnImprimir.addEventListener('click', function () {
+
+    validarCampoVacio(txtCveFamiliar);
+});
+
+window.addEventListener('load', function () {
+    verificarLlenadoPrescripciones();
+});
+
+function validarCampoVacio(input) {
+    const valor = input.value.trim();
+    const nombre = 'error' + input.id;
+
+    if (valor === '') {
+        document.getElementById(nombre).textContent = 'No puede estar vacío';
+    } else {
+        document.getElementById(nombre).textContent = '';
+    }
+}
+
